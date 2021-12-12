d1803a7881591dbcd1c6900fe14b1c74e3015168
Faris Naji <fpnaji@gmail.com>
2021-12-12 17:01:01 +0000

    init the repo

--- main.R
+++ main.R
@@ -1,11 +1,39 @@
 library(tercen)
-library(dplyr, warn.conflicts = FALSE)
+library(tidyr)
+library(dplyr)
+
 
 ctx = tercenCtx()
 
-ctx %>%
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(mean = mean(.y)) %>%
+if (!any(ctx$cnames == "documentId")) stop("Column factor documentId is required")
+
+# extract files
+df <- ctx$cselect()
+
+docId = df$documentId[1]
+doc = ctx$client$fileService$get(docId)
+filename = tempfile()
+writeBin(ctx$client$fileService$download(docId), filename)
+on.exit(unlink(filename))
+
+img <- png::readPNG(filename)
+
+matrix <- img[,,1]
+
+col_num <- 1:ncol(matrix)
+row_num <- 1:nrow(martix)
+
+col_id <-sprintf(paste0("c%0", max(nchar(as.character(col_num))), "d"), col_num)
+row_id <-sprintf(paste0("r%0", max(nchar(as.character(row_num))), "d"), row_num)
+
+colnames(matrix) <- col_id
+rownames(matrix) <- row_id
+
+matrix_table <- as_tibble(matrix) %>% 
+  mutate(row_id = row_id) %>%
+  pivot_longer(-row_id, names_to ="col_id", values_to = "pixel_value") %>%
+  mutate_if(is.integer, as.double) %>%
+  mutate(filename = basename(tmpdir)) %>%
+  mutate(.ci = 0) %>%
   ctx$addNamespace() %>%
   ctx$save()
--- workspace.R
+++ workspace.R
@@ -1,17 +1,49 @@
 library(tercen)
 library(dplyr, warn.conflicts = FALSE)
+library(tidyr)
 
-options("tercen.workflowId" = "wwww")
-options("tercen.stepId"     = "dddd")
+
+options("tercen.workflowId" = "3ac390fd32fea6c2f28ea5a014000c70")
+options("tercen.stepId"     = "008be523-7d4f-459c-b3f8-ca0ab87f28ae")
 
 getOption("tercen.workflowId")
 getOption("tercen.stepId")
+getOption("tercen.serviceUri")
+
+options("tercen.serviceUri"="http://tercen:5400/api/v1/")
+options("tercen.username"= "admin")
+options("tercen.password"= "admin")
 
 ctx = tercenCtx()
 
-ctx %>%
-  select(.y, .ci, .ri) %>%
-  group_by(.ci, .ri) %>%
-  summarise(mean = mean(.y)) %>%
+if (!any(ctx$cnames == "documentId")) stop("Column factor documentId is required")
+
+# extract files
+df <- ctx$cselect()
+
+docId = df$documentId[1]
+doc = ctx$client$fileService$get(docId)
+filename = tempfile()
+writeBin(ctx$client$fileService$download(docId), filename)
+on.exit(unlink(filename))
+
+img <- png::readPNG(filename, native = TRUE)
+
+matrix <- matrix(img, byrow=TRUE, nrow=nrow(img))
+
+col_num <- 1:ncol(matrix)
+row_num <- 1:nrow(matrix)
+
+col_id <-sprintf(paste0("c%0", max(nchar(as.character(col_num))), "d"), col_num)
+row_id <-sprintf(paste0("r%0", max(nchar(as.character(row_num))), "d"), row_num)
+
+colnames(matrix) <- col_id
+rownames(matrix) <- row_id
+
+matrix_table <- as_tibble(matrix) %>% 
+  mutate(row_id = row_id) %>%
+  pivot_longer(-row_id, names_to ="col_id", values_to = "pixel_value") %>%
+  mutate_if(is.integer, as.double) %>%
+  mutate(.ci = 0) %>%
   ctx$addNamespace() %>%
   ctx$save()
